be57a7100ffe8fcfdd1284b5918fb29188d8db65
Chris Macharia <Christophermaina003@gmail.com>
2023-08-23 06:13:59 +0300

    Create a database if missing

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- This script uses the CREATE DATABASE IF NOT EXISTS statement to avoid failure if the database already exists
+
+-- Create the database if it doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE hbtn_0c_0;
+
+-- Script executed successfully
