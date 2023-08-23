ac22719178fe1e6b5448f06d4be07ff9d8a8b6ba
Chris Macharia <Christophermaina003@gmail.com>
2023-08-23 06:16:04 +0300

    Delete database in the server

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script to drop the database hbtn_0c_0 if it exists
+-- This script uses the DROP DATABASE IF EXISTS statement to avoid failure if the database doesn't exist
+
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
